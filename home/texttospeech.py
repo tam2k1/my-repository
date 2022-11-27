25dee4c7e756fec708fbf3640a04bd870267e3dc
Truong Tri Tam <105215666+tam2k1@users.noreply.github.com>
2022-11-27 08:39:20 +0700

    Delete home directory

--- home/texttospeech.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import playsound
-
-def texttospeech(text):
-    output = gTTS(text,lang="vi", slow=False)
-    output.save("output.mp3")
-    playsound.playsound('output.mp3', True)
